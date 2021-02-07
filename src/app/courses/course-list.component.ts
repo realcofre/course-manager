bbe390eb31d96ef8c73b8dbd3e2445a6964271d0
realcofre <renatoacf@gmail.com>
2021-02-07 18:13:40 -0300

    Course List e Rating

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,10 +1,38 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
 
 @Component({
     selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
 
-export class CourseListComponent {
+export class CourseListComponent implements OnInit {
+
+    courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageUrl: '/assets/images/forms.png',
+                price: 99.99,
+                code: 'XPS-7898',
+                duration: 120,
+                rating: 4.5,
+                releaseDate: 'December, 2, 2019'
+            },
+            {
+                id: 2,
+                name: 'Angular: HTTP',
+                imageUrl: '/assets/images/http.png',
+                price: 45.99,
+                code: 'XPS-1098',
+                duration: 80,
+                rating: 4,
+                releaseDate: 'December, 4, 2019'
+            }
+        ]
+    }
 
 }
